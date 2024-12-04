2d219ca3fca63debea5844b0780292dde5c8d2df
Daniel Indrieri <indrieridaniel@gmail.com>
2024-12-04 16:07:43 +0100

    adds

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import logo from './logo.svg';
-import wanda from './data/wanda.jpg'
+import wanda from './data/wanda.jpg';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import MyNav from './Components/MyNav';
 import MyMain from './Components/MyMain';
 import MyMain2 from './Components/MyMain2';
 import MyMain3 from './Components/MyMain3';
 import MyFooter from './Components/MyFooter';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <div>
         <header>
-          <MyNav logo={logo} wanda={wanda}/>
+          <MyNav logo={logo} wanda={wanda} />
         </header>
-        <main className='bg-dark'>
-          <MyMain/>
-          <MyMain2/>
-          <MyMain3/>
-          </main>
+        <main className="bg-dark">
+          <Routes>
+            {/* Rotta per la homepage */}
+            <Route
+              path="/"
+              element={
+                <Row className="justify-content-center mt-3">
+                  <Col xs={12} md={8} lg={6}>
+                    <MyMain />
+                    <MyMain2 />
+                    <MyMain3 />
+                  </Col>
+                </Row>
+              }
+            />
+          </Routes>
+        </main>
         <footer>
-        <MyFooter/>
+          <MyFooter />
         </footer>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-//Ciao Stefano, questa sarà il primo progetto piu bello ma allo stesso tempo quello che mi ha reso piu insicuro di me.
-// se non fosse stato per l'aiuto di qualche collega,le tue Repository e un aiuto da chatgpt forse non sarei riuscito a fare nemmeno il 20 % del progetto
-// la logica ancora non è entrata in testa ma non mi fermo di certo qui,cerco sempre di dare il mio meglio nei compiti del pommeriggio ma è sempre poco
-// naturalmente se metto in confronto quello che facevo 2 mesetti fa ad oggi è tantissima roba(per uno che non ha mai messo mano ad un codice)
-// spero di migliorare il piu possibile,non voglio essere il migliore...voglio essere la miglior versione di me stesso ogni giorno e apprendere il piu possibile da te e dal corso
-// Non ti rubo altro tempo ci tenevo solo a dirti che apprezzo il tuo lavoro e che spero un giorno di poter fare un progetto come questo interamente solo con le mie forze,grazie e scusami se sono stato poco formale nei tuoi confronti 
--- src/Components/MyNav.jsx
+++ src/Components/MyNav.jsx
@@ -1,6 +1,9 @@
 import {Nav,Navbar,Container} from 'react-bootstrap';
+import { useLocation, Link } from 'react-router-dom';
+
 
 function MyNav({logo,wanda}) {
+  const location = useLocation();
   return (
     <Navbar expand="lg" className="bg-dark" fixed='top'>
       <Container fluid className='m-0'>
@@ -13,21 +16,46 @@ function MyNav({logo,wanda}) {
         <Navbar.Toggle  className="text-dark bg-white" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="text-white d-flex justify-content-center justify-content-lg-between " id="basic-navbar-nav">
           <Nav className="bg-dark text-white ">
+            <Link to="/"
+              className={
+                location.pathname === '/MyMain' ? 'nav-link active' : 'nav-link'
+              }>
             <Nav.Link className="color text-center" href="#home">
               Home
             </Nav.Link>
-            <Nav.Link className="color text-center" href="#link">
+            </Link>
+            <Link to="/"
+              className={
+                location.pathname === '/MyMain2' ? 'nav-link active' : 'nav-link'
+              }>
+            <Nav.Link className="color text-center">
               TV Shows
             </Nav.Link>
+            </Link>
+            <Link to="/MyMain3"
+              className={
+                location.pathname === '/' ? 'nav-link active' : 'nav-link'
+              }>
             <Nav.Link className="color text-center" href="#link">
               Movies
             </Nav.Link>
+            </Link>
+            <Link to="/"
+              className={
+                location.pathname === '/MyFooter' ? 'nav-link active' : 'nav-link'
+              }>
             <Nav.Link className="color text-center" href="#link">
               Recently Added
             </Nav.Link>
+            </Link>
+            <Link to="/"
+              className={
+                location.pathname === '/' ? 'nav-link active' : 'nav-link'
+              }>
             <Nav.Link className="color text-center" href="#link">
               My List
             </Nav.Link>
+            </Link>
           </Nav>
           <div className='me-1 d-flex align-items-center'>
           <i class="bi bi-search me-3"></i>
